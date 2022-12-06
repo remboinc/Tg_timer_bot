4ff86b68238a4da38ca98fb764d31c4c60d8e6cb
Ekaterina Ryabova <remboinc@mail.ru>
2022-12-06 12:51:38 +0300

    Delete defmain

--- main.py
+++ main.py
@@ -7,9 +7,8 @@ from pytimeparse import parse
 def wait(chat_id, time):
     secs_left = parse(time)
     start_timer = "Запускаю таймер"
-    time_left = f'Осталось {secs_left} секунд из {secs_left}'
     bot.send_message(chat_id, start_timer)
-    message_id = bot.send_message(chat_id, time_left)
+    message_id = bot.send_message(chat_id, secs_left)
     bot.create_countdown(parse(time), notify_progress, message_id=message_id, time=time)
     bot.create_timer(parse(time), choose, chat_id=chat_id)
 
@@ -21,9 +20,7 @@ def notify_progress(secs_left, message_id, time):
     bot.update_message(tg_chat_id, message_id, progressbar)
 
 
-
-def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='█', zfill='░'):
->>>>>>> 3a625f990b0640aeac233f53e50103f6e337378e
+def render_progressbar(total, iteration, prefix='', suffix='', length=13, fill='█', zfill='░'):
     iteration = min(total, iteration)
     percent = "{0:.1f}"
     percent = percent.format(100 * (iteration / float(total)))
@@ -33,16 +30,11 @@ def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='
 
 
 def choose(chat_id):
-    message = "Время вышло"
+    time_is_over = "Время вышло"
+    bot.send_message(chat_id, time_is_over)
+    message = 'На сколько поставить таймер?'
     bot.send_message(chat_id, message)
 
-
-def main(chat_id, time):
-    if type(parse(time)) == int:
-        wait(chat_id, time)
-    else:
-        message = 'На сколько поставить таймер?'
-        bot.send_message(chat_id, message)
 
 
 if __name__ == '__main__':
@@ -51,5 +43,5 @@ if __name__ == '__main__':
     tg_chat_id = os.getenv("TG_CHAT_ID")
     bot = ptbot.Bot(tg_token)
 
-    bot.reply_on_message(main)
+    bot.reply_on_message(wait)
     bot.run_bot()
